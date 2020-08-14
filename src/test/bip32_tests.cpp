265501186b6bc8ff6cd15078baafe417cfad3426
TFinch <nexusnow@protonmail.com>
2020-08-14 00:30:29 -0400

    test

--- src/test/bip32_tests.cpp
+++ src/test/bip32_tests.cpp
@@ -75,8 +75,8 @@ void RunTest(const TestVector& test)
     std::vector<unsigned char> seed = ParseHex(test.strHexMaster);
     CExtKey key;
     CExtPubKey pubkey;
-    key.SetMaster(&seed[0], seed.size());
-    pubkey = key.Neuter();
+    key.SetSeed(seed.data(), seed.size());
+    pubkey = key.Neutered();
     for (const TestDerivation& derive : test.vDerive) {
         unsigned char data[74];
         key.Encode(data);
